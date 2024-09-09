70b93ac0cbc34a7f959e521a089f8b680e0fe7ec
Patrix <champion790713@gmail.com>
2024-09-09 16:43:38 +0800

    [34] target at the last position of three

--- TDDLeetCodeTests/LeetCode34Tests.swift
+++ TDDLeetCodeTests/LeetCode34Tests.swift
@@ -76,6 +76,7 @@ final class LeetCode34Tests: XCTestCase {
         XCTAssertEqual(searchRange([5,6,7], 3), defaultFailure)
         XCTAssertEqual(searchRange([1,2,3], 1), [0, 0])
         XCTAssertEqual(searchRange([1,2,3], 2), [1, 1])
+        XCTAssertEqual(searchRange([1,2,3], 3), [2, 2])
     }
     
     private let defaultFailure = [-1, -1]
