a8afec043e6afdae79c602688b40020cdb321af4
Patrix <champion790713@gmail.com>
2024-09-09 16:20:59 +0800

    [34] happy path with two valid elements

--- TDDLeetCodeTests/LeetCode34Tests.swift
+++ TDDLeetCodeTests/LeetCode34Tests.swift
@@ -16,7 +16,23 @@ func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
         return nums[0] == target ? [0, 0] : [-1, -1]
     }
     
-    return [-1, -1]
+    var start = 0
+    var end = nums.count-1
+    var result = [Int]()
+    
+    while (start <= end) {
+        if nums[start] == target {
+            result.append(start)
+            start += 1
+        }
+        
+        if nums[end] == target {
+            result.append(end)
+            end -= 1
+        }
+    }
+    
+    return result
 }
 
 final class LeetCode34Tests: XCTestCase {
@@ -28,6 +44,10 @@ final class LeetCode34Tests: XCTestCase {
     func test_onlyOneNum_withCorrectResult() {
         XCTAssertEqual(searchRange([1], 1), [0, 0])
         XCTAssertEqual(searchRange([0], 2), defaultFailure)
+    }
+    
+    func test_twoNums_withCorrectResult() {
+        XCTAssertEqual(searchRange([2,2], 2), [0, 1])
     }
     
     private let defaultFailure = [-1, -1]
