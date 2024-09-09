ba099a37835ade9c77fe2cb2db2930fbfcfcd273
Patrix <champion790713@gmail.com>
2024-09-09 16:06:17 +0800

    Initial Commit

---
+++ TDDLeetCode/TDDLeetCode.h
@@ -0,0 +1,18 @@
+//
+//  TDDLeetCode.h
+//  TDDLeetCode
+//
+//  Created by Champion Chen on 2024/9/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDDLeetCode.
+FOUNDATION_EXPORT double TDDLeetCodeVersionNumber;
+
+//! Project version string for TDDLeetCode.
+FOUNDATION_EXPORT const unsigned char TDDLeetCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDLeetCode/PublicHeader.h>
+
+
