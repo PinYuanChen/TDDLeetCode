d120754034c5b2b1d246b9773985176b6f189564
Patrix <champion790713@gmail.com>
2024-09-09 16:27:22 +0800

    [34] test with a target number between two numbers

--- TDDLeetCodeTests/LeetCode34Tests.swift
+++ TDDLeetCodeTests/LeetCode34Tests.swift
@@ -30,9 +30,16 @@ func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
             result.append(end)
             end -= 1
         }
+        
+        let mid = (start+end)/2
+        if nums[mid] > target {
+            end = mid - 1
+        } else if nums[mid] < target {
+            start = mid + 1
+        }
     }
     
-    return result
+    return result.isEmpty ? [-1, -1] : result
 }
 
 final class LeetCode34Tests: XCTestCase {
@@ -48,6 +55,7 @@ final class LeetCode34Tests: XCTestCase {
     
     func test_twoNums_withCorrectResult() {
         XCTAssertEqual(searchRange([2,2], 2), [0, 1])
+        XCTAssertEqual(searchRange([0,4], 3), defaultFailure)
     }
     
     private let defaultFailure = [-1, -1]
