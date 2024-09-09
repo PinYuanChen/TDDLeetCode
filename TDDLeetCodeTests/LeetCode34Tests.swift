c8a0f74a60cb57ed9264b7846590d101567eca0a
Patrix <champion790713@gmail.com>
2024-09-09 17:17:55 +0800

    [34] test first three

--- TDDLeetCodeTests/LeetCode34Tests.swift
+++ TDDLeetCodeTests/LeetCode34Tests.swift
@@ -101,6 +101,7 @@ final class LeetCode34Tests: XCTestCase {
         XCTAssertEqual(searchRange([1,1,3,4], 1), [0, 1])
         XCTAssertEqual(searchRange([1,2,2,4], 2), [1, 2])
         XCTAssertEqual(searchRange([1,2,3,3], 3), [2, 3])
+        XCTAssertEqual(searchRange([1,1,1,4], 1), [0, 2])
     }
     
     private let defaultFailure = [-1, -1]
